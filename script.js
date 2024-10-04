1981313e00dda95714a58a63fa9b1e1492d5469f
SkylerRo <Skylerro20@gmail.com>
2024-10-03 22:41:34 -0400

    Tic-Tac-Toe

---
+++ script.js
@@ -0,0 +1,85 @@
+
+
+
+const cells = document.querySelectorAll(".cell")
+const statusText = document.querySelector("#statusText")
+const restartButton = document.querySelector("#restartButton")
+const winConditions = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+];
+let choices = ["", "", "", "", "", "", "", "", ""]
+let currentPlayer = "X"
+let running = false
+
+initializeGame()
+
+function initializeGame() {
+    cells.forEach(cell => cell.addEventListener("click", cellClicked))
+    restartButton.addEventListener("click", restartGame)
+    statusText.textContent = `${currentPlayer}'s turn`
+    running = true
+}
+function cellClicked() {
+    const cellIndex = this.getAttribute("cellIndex")
+
+    if(choices[cellIndex] != "" || !running) {
+        return
+    }
+
+    updateCell(this, cellIndex)
+    checkWinner()
+}
+function updateCell(cell, index) {
+    choices[index] = currentPlayer
+    cell.textContent = currentPlayer
+}
+function changePlayer() {
+    currentPlayer = (currentPlayer == "X") ? "O" : "X"
+    statusText.textContent = `${currentPlayer}'s turn`
+}
+function checkWinner() {
+    let roundWon = false
+
+    for(let i = 0; i < winConditions.length; i++){
+        const condition = winConditions[i]
+        const cellA = choices[condition[0]]
+        const cellB = choices[condition[1]]
+        const cellC = choices[condition[2]]
+
+        if(cellA == "" || cellB == "" || cellC == "") {
+            continue
+        }
+        if(cellA == cellB && cellB == cellC) {
+            roundWon = true
+            break
+        }
+    }
+
+    if(roundWon){
+        statusText.textContent = `${currentPlayer} wins!`
+        running = false
+    }
+    else if(!choices.includes("")){
+        statusText.textContent = `Draw!`
+        running = false
+    }
+    else{
+        changePlayer()
+    }
+}
+function restartGame(){
+    currentPlayer = "X"
+    choices = ["", "", "", "", "", "", "", "", ""]
+    statusText.textContent = `${currentPlayer}'s turn`
+    cells.forEach(cell => cell.textContent = "")
+    running = true
+}
+
+
